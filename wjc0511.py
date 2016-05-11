2d2a1d88d6a70fa53a18cfd33ba719a25681d5a3
王佳灿 <wjc0216wjc>
2016-05-11 23:05:42 +0800

    shawncan
    测试图片

---
+++ wjc0511.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from PIL import Image
+
+im = Image.open('/Users/wangjiacan/Desktop/1.jpg')
+w, h = im.size
+print('Original image size: %sx%s' % (w, h))
+
+im.thumbnail((w//2, h//2))
+print('Resize image to: %sx%s' % (w//2, h//2))
+
+im.save('thumbnail.jpg', 'jpeg')
