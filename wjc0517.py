e455c82b49a978c632881a5bfee3b768c1e1bd7b
王佳灿 <wjc0216wjc>
2016-05-26 10:47:52 +0800

    shawncan
    模拟登陆

--- wjc0517.py
+++ wjc0517.py
@@ -11,15 +11,15 @@ login_data = parse.urlencode([
     ('username', email),
     ('password', passwd),
     ('savestate', '1'),
-    ('ec', '1'),    
+    ('ec', ''),    
     ('pagerefer', 'https://passport.weibo.cn/signin/welcome?entry=mweibo&r=http%3A%2F%2Fm.weibo.cn%2F'),
     ('entry', 'mweibo')
     ])
 
-req = request.Request('https://passport.weibo.cn/signin')
+req = request.Request('https://passport.weibo.cn/sso/login')
 req.add_header('Origin', 'https://passport.weibo.cn')
 req.add_header('User-Agent', 'Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25')
-req.add_header('Referer', 'https://passport.weibo.cn/signin/welcome?entry=mweibo&r=http%3A%2F%2Fm.weibo.cn%2F')
+req.add_header('Referer', 'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=http%3A%2F%2Fm.weibo.cn%2F')
 '''
 with request.urlopen(req, data=login_data.encode('utf-8')) as f:
     print('Status:', f.status, f.reason)
