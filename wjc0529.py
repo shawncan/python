3666b51ee604fef68f7582a7733d4d601ced5140
王佳灿 <wjc0216wjc>
2016-05-29 22:00:08 +0800

    shawncan
    web开发

---
+++ wjc0529.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#制作一个WSGI接口
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    body = '<h1>Hello, %s </h1>' % (environ['PATH_INFO'][1:] or 'web')
+    return[body.encode('utf-8')]
+    #return[b'<h1>Hello, web!</h1>']
+
