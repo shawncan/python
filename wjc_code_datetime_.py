d9f564e874d1bf479b073ee3afa58bcdb6aff220
王佳灿 <wjc0216wjc>
2016-08-07 22:06:44 +0800

    shawncan
    code时间日期模块学习

---
+++ wjc_code_datetime_.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/Python3
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+now = datetime.now()
+print(now)
+
+print(now.year)
+print(now.month)
+print(now.day)
+print(now.time)
