bc4ec909747bf94a49204c9931aa7101702909f6
王佳灿 <wjc0216wjc>
2016-08-08 23:56:42 +0800

    shawncan
    测试了下time模块打印时间与格式化时间

--- wjc_code_datetime_.py
+++ wjc_code_datetime_.py
@@ -1,12 +1,26 @@
-#!/usr/local/bin/Python3
+#!/usr/local/bin/Python3"default_encoding": "UTF-8",
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
+# from datetime import datetime
 
-now = datetime.now()
-print(now)
+# now = datetime.now()
+# print(now)
+# print("当前时间: %s" % now)
 
-print(now.year)
-print(now.month)
-print(now.day)
-print(now.time)
+# print(now.year)
+# print(now.month)
+# print(now.day)
+# print(now.hour)
+# print(now.min)
+
+'''
+#也可以用time模块来查看当前的时间与提取出当前的时间日期
+import time
+
+print(time.localtime())
+
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+print("当前时间：%s " % now)
+'''
