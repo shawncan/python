3da2007f0af278c53dc6ea5976704f1917219f18
王佳灿 <wjc0216wjc>
2016-08-14 20:08:39 +0800

    shawncan
    条件控制表达式：布尔运算符，计算顺序、条件判断语句

--- wjc_code_datetime_Conditionals.py
+++ wjc_code_datetime_Conditionals.py
@@ -2,16 +2,38 @@
 # -*- coding: utf-8 -*-
 
 #20160811
-def clinic():
-    print("You've just entered the clinic!")
-    print("Do you take the door on the left or the right?")
-    answer = input("Type left or right and hit 'Enter'.").lower()
-    if answer == "left" or answer == "l":
-        print("This is the Verbal Abuse Room, you heap of parrot droppings!")
-    elif answer == "right" or answer == "r":
-        print("Of course this is the Argument Room, I've told you that already!")
-    else:
-        print("You didn't pick left or right! Try again.")
-        clinic()
+# def clinic():
+#     print("You've just entered the clinic!")
+#     print("Do you take the door on the left or the right?")
+#     answer = input("Type left or right and hit 'Enter'.").lower()
+#     if answer == "left" or answer == "l":
+#         print("This is the Verbal Abuse Room, you heap of parrot droppings!")
+#     elif answer == "right" or answer == "r":
+#         print("Of course this is the Argument Room, I've told you that already!")
+#     else:
+#         print("You didn't pick left or right! Try again.")
+#         clinic()
 
-n = clinic()
+# n = clinic()
+
+
+#20160814-条件控制表达式
+answer = "'Tis but a scratch!"
+
+def black_knight():
+    if answer == "'Tis but a scratch!":
+        return True
+    else:             
+        return "wjc"        # Make sure this returns False
+
+def french_soldier():
+    if answer == "Go away, or I shall taunt you a second time!":
+        return True
+    else:             
+        return "xxx"        # Make sure this returns False
+    
+n = black_knight()
+b = french_soldier()
+
+print(n)
+print(b)
