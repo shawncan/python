2768a789a20c453b6dbd96c6f612a26d780a46d2
王佳灿 <wjc0216wjc>
2016-08-22 22:05:00 +0800

    shawncan
    优化

--- wjc_vehiclerecord/vere_licenseplate.py
+++ wjc_vehiclerecord/vere_licenseplate.py
@@ -5,36 +5,27 @@ import random, string
 
 # 随机生成车牌地区编码Coding region
 def region():
-    reg = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L"]
-    return random.choice( reg )
+    reg = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", 'L']
+    return random.choice(reg)
 
 
 # 随机生成车牌到后2位
 def second(size=2, chars=string.ascii_uppercase + string.digits):
-    sec = ''.join( random.choice( chars ) for _ in range( size ) )
-    if sec[0] == sec[1]:
-        # print("Generating tail number error")
+    end = ''.join(random.choice(chars) for _ in range(size))
+    if end[0] == end[1]:
+        print("Error: repeat tail number")
         exit()
     else:
-        return sec
+        if end =="I" or end == "O":
+            print("Generating License Plate error")
+            exit()
+        else:
+            return end
 
 
-# 生成车牌
-def plate(region, second):
-    if second == "I" or second == "O":
-        # print("Generating License Plate error")
-        exit()
-    else:
-        # print("浙%s***%s" % (region, second))
-        licen = ("浙%s***%s" % (region, second))
-        return licen
-
-# def generate():
-#     with open('test.txt', 'a') as test:
-#         test.write('\n' + licenseplate)
-#         test.close()
 
 region = region()
 second = second()
-licenseplate = plate(region, second)
-# gener = generate()
+
+# 生成车牌
+licenseplate = ("浙%s***%s" % (region, second))
