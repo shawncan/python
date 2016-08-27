d8b08f1611675b283d01ff1b5d6396a18572be7e
王佳灿 <wjc0216wjc>
2016-08-27 09:43:39 +0800

    shawncan
    优化代码

--- wjc_vehiclerecord/vere_licenseplate.py
+++ wjc_vehiclerecord/vere_licenseplate.py
@@ -3,6 +3,7 @@
 
 import random, string
 
+
 # 随机生成车牌地区编码Coding region
 def region():
     reg = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", 'L']
@@ -15,17 +16,16 @@ def second(size=2, chars=string.ascii_uppercase + string.digits):
     if end[0] == end[1]:
         print("Error: repeat tail number")
         exit()
+    elif end == "I" or end == "O":
+        print("Generating License Plate error")
+        exit()
     else:
-        if end =="I" or end == "O":
-            print("Generating License Plate error")
-            exit()
-        else:
-            return end
-
-
+        return end
 
 region = region()
 second = second()
 
 # 生成车牌
 licenseplate = ("浙%s***%s" % (region, second))
+
+print(licenseplate)
