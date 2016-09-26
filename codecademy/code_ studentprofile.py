c83a1285e648c3541d44133cf86447b3c00e7e15
王佳灿 <wjc0216wjc>
2016-09-26 23:39:35 +0800

    shawncan
    列表与字典的练习-完成

--- codecademy/code_ studentprofile.py
+++ codecademy/code_ studentprofile.py
@@ -20,19 +20,57 @@ tyler = {
     "tests": [100.0, 100.0]
 }
 
+students =  [lloyd, alice, tyler]
+
 # def average(numbers):
 #     total = sum(numbers)
 #     total = float(total)
 #     total = total / len(numbers)
 #     return total
 
+#一门课的平均数
 def average(numbers):
     total = float(sum(numbers)) / len(numbers)
     return total
 
+#学生的平均分
 def get_average(student):
     homework = average(student["homework"])
     quizzes = average(student["quizzes"])
     tests = average(student["tests"])
     total = 0.1 * homework + 0.3 * quizzes + 0.6 * tests
     return total
+
+#学生的成绩
+def get_letter_grade(score):
+    if score >= 90:
+        return "A"
+    elif 90 > score >= 80:
+        return "B"
+    elif 80 > score >= 70:
+        return "C"
+    elif 70 > score >= 60:
+        return "D"
+    else:
+        return "F"
+
+#多个学生的平均分
+def get_class_average(students):
+    resudents = []
+    for student in students:
+        resudents.append(get_average(student))
+    return average(resudents)
+
+#alice的平均分与成绩
+average_score = get_average(alice)
+achievement = get_letter_grade(average_score)
+print("alice的平均分：%.2f" % average_score)
+print("alice的成绩：%s" % achievement)
+
+#705班的平均分成绩
+class_average_score = get_class_average(students)
+class_achievement = get_letter_grade(class_average_score)
+print("705班级平均分：%.2f" % class_average_score)
+print("705班级成绩：%s" % class_achievement)
+
+
