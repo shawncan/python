ac228061a60bdb2b0e1a196a8b47daa47bb95a30
王佳灿 <wjc0216wjc>
2016-09-29 23:05:26 +0800

    shawncan
    优化了循环打印次数由手动输入！

--- wjc_vehiclerecord/vehicle_information.py
+++ wjc_vehiclerecord/vehicle_information.py
@@ -4,33 +4,33 @@ import random, string, time
 
 
 # 随机生成车牌地区编码
-def region():
-    reg = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", 'L']
-    return random.choice(reg)
+def letter():
+    coding = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", 'L']
+    return random.choice(coding)
 
 
-# 随机生成车牌到后2位
-def second(size=2, chars=string.ascii_uppercase + string.digits):
-    end = ''.join(random.choice(chars) for _ in range(size))
-    if end[0] == end[1]:
+# 随机生成车牌最后2位
+def tail_number(size=2, chars=string.ascii_uppercase + string.digits):
+    tail = ''.join(random.choice(chars) for _ in range(size))
+    if tail[0] == tail[1]:
         print("Error: repeat tail number")
         exit()
-    elif end == "I" or end == "O":
+    elif tail == "I" or tail == "O":
         print("Generating License Plate error")
         exit()
     else:
-        return end
+        return tail
 
 
 # 随机生成车牌
-def license():
-    b = region()
-    c = second()
-    licenseplate = ("浙%s***%s" % (b, c))
+def license_plate():
+    coding = letter()
+    figure = tail_number()
+    licenseplate = ("浙%s***%s" % (coding, figure))
     return licenseplate
 
 
-# 随机生成时间
+# 随机购买生成时间
 def time_purchase():
     data = time.strftime("%Y-%m-%d ", time.localtime())
     period = time.strftime("%H:%M:%S", time.localtime(random.randint(1471485600, 1471514400)))
@@ -40,14 +40,16 @@ def time_purchase():
 
 # 随机生成数据
 def generate():
-    license_plate = license()
-    period = time_purchase()
+    licenseplate = license_plate()
+    timepurchase = time_purchase()
     fee = random.randint(1000, 2000)
     with open('/Users/wangjiacan/Desktop/cesi.txt', 'a') as test:
-        test.write('\n' + license_plate + '  ' + period + '  ￥' + str(fee))
+        test.write('\n' + licenseplate + '  ' + timepurchase + '  ￥' + str(fee))
         test.close()
 
-for i in range(10):
+frequency = input("请输入输出次数:")
+
+for i in range(int(frequency)):
     generate()
 
 print("程序运行成功,快去看看吧!!!")
