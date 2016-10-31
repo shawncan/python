252b87650d5bd084b6d53474e3cde9cc26a7382e
王佳灿 <wjc0216wjc>
2016-10-31 21:30:36 +0800

    shawncan
    更新

--- codecademy/wjc_code_battleship.py
+++ codecademy/wjc_code_battleship.py
@@ -21,23 +21,33 @@ def random_row(board):
 def random_col(board):
     return randint(0, len(board) - 1)
 
+print("Let's play Battleship!")
+print_board(board)
 
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-guess_row = int(input('Guess Row: '))
-guess_col = int(input("Guess Col: "))
-print("Ship Row: %s" % ship_row)
-print("Ship Col: %s" % ship_col)
+for turn in range(4):
+    guess_row = int(input('Guess Row: '))
+    guess_col = int(input("Guess Col: "))
 
-if guess_row == ship_row and guess_col == ship_col:
-    print("Congratulations! You sank my battleship!")
-elif guess_row == ship_row or guess_col == ship_col:
-    print("You guessed that one already.")
-else:
-    if guess_row not in range(5) or guess_col not in range(5):
-        print("Oops, that's not even in the ocean.")
+    print("Ship Row: %s" % ship_row)
+    print("Ship Col: %s" % ship_col)
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congratulations! You sank my battleship!")
+        break
+    elif guess_row == ship_row or guess_col == ship_col:
+        print("You guessed that one already.")
     else:
-        print("You missed my battleship! battleship here!")
-        board[guess_row][guess_col] = "X"
-        print(print_board(board))
+        if guess_row not in range(5) or guess_col not in range(5):
+            print("Oops, that's not even in the ocean.")
+        # elif board[guess_row][guess_col] == "X":
+        #     print("You guessed that one already.")
+        else:
+            print("You missed my battleship! battleship here!")
+    board[guess_row][guess_col] = "X"
+    print(turn + 1)
+    print(print_board(board))
+    if turn == 3:
+        print("Game Over")
