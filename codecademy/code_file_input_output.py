7ef1af1ec04f6f878c58f0da8a1bba010116e222
shawncan <wangjiacan0216@gmail.com>
2016-11-19 15:40:00 +0800

    文件的读写

--- codecademy/code_file_input_output.py
+++ codecademy/code_file_input_output.py
@@ -5,8 +5,12 @@ my_list = [i**2 for i in range(1, 11)]
 
 my_file = open("output.txt", "r+")
 
-for i in my_list:
-    my_file.write(str(i))
-    my_file.write("\n")
+# for i in my_list:
+#     my_file.write(str(i))
+#     my_file.write("\n")
+print(my_file.readline())
+print(my_file.readline())
+print(my_file.readline())
+print(my_file.readline())
 my_file.close()
 print("打印完成")
