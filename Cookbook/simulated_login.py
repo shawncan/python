2a24dcbd852f70ed323350364b5a5b7f3d2692b5
shawncan <wangjiacan0216@gmail.com>
2016-11-26 17:02:40 +0800

    模拟登录_20161126

--- Cookbook/simulated_login.py
+++ Cookbook/simulated_login.py
@@ -59,39 +59,45 @@ import http.client
 # print(res.msg)
 # print(res.getheader('Set-Cookie'))
 
-
 """模拟人人网登录2"""
 
-filename = "cookie.txt"
-cookie = http.cookiejar.MozillaCookieJar(filename)
-opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
-postdata = urllib.parse.urlencode({
-    "email": "784241389@qq.com",
-    "password": "920216+wjc",
-})
-binary_data = postdata.encode('utf-8')
+# filename = "cookie.txt"
+# cookie = http.cookiejar.MozillaCookieJar(filename)
+# opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
+# postdata = urllib.parse.urlencode({
+#     "email": "784241389@qq.com",
+#     "password": "920216+wjc",
+# })
+# binary_data = postdata.encode('utf-8')
+#
+# headers = {
+#     'Content-type': 'application/x-www-form-urlencoded',
+#     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko)",
+#     "Referer": "http://www.renren.com/"
+# }
+#
+# url = "http://www.renren.com"
+# request = urllib.request.Request(url, binary_data, headers)
+# result = opener.open(request)
+# # print(str(result.read(), "utf-8"))
+# cookie.save(ignore_discard=True, ignore_expires=True)
+# print("获取完成")
+# print(result.getcode())
 
-headers = {
-    'Content-type': 'application/x-www-form-urlencoded',
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko)",
-    "Referer": "http://www.renren.com/"
-}
-
-url = "http://www.renren.com"
-request = urllib.request.Request(url, binary_data, headers)
-result = opener.open(request)
-# print(str(result.read(), "utf-8"))
-cookie.save(ignore_discard=True, ignore_expires=True)
-print("获取完成")
+"""使用cookie登录"""
 
 # cookie = http.cookiejar.MozillaCookieJar()
 # cookie.load("cookie.txt", ignore_discard=True, ignore_expires=True)
-# for item in cookie:
-#     print("name:" + item.name + "-value:" + item.value)
-# handler = urllib.request.HTTPCookieProcessor(cookie)
-# opener = urllib.request.build_opener(handler)
-#
-# get_url = 'http://friend.renren.com/managefriends'
-# get_request = urllib.request.Request(get_url)
-# get_response = opener.open(get_request)
-# print(get_response.read().decode())
+# req = urllib.request.Request("http://www.renren.com")
+# opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
+# response = opener.open(req)
+# print(str(response.read(),  "utf-8"))
+# print(response.getcode())
+
+# HEADERS = {"cookie": "anonymid=ivyxbcmv-j74tlj; domain=.renren.com; path=/; expires=Thu, 25-Nov-2021 07:56:52 GMT, _de=797DD4BF9FBE00BEE2A69D838E8B6F33696BF75400CE19CC; domain=.renren.com; path=/; expires=Tue, 21-Nov-2017 07:56:52 GMT, p=990d900e20f80622e8e26db4f7aaf8e12; domain=renren.com; path=/, first_login_flag=1; domain=renren.com; path=/, ln_uact=784241389@qq.com; domain=renren.com; path=/; expires=Mon, 26-Dec-2016 07:56:52 GMT, ln_hurl=http://hdn.xnimg.cn/photos/hdn521/20140316/2040/h_main_KoEI_81780000021f113e.jpg; domain=renren.com; path=/; expires=Mon, 26-Dec-2016 07:56:52 GMT, t=d8238b18f384aec6a4f89346c571d0ee2; domain=.renren.com; path=/, t=b2b28e432b764e4f883bb04abb7362e4; domain=renren.com; path=/xtalk/, societyguester=d8238b18f384aec6a4f89346c571d0ee2; domain=.renren.com; path=/, id=400078482; domain=.renren.com; path=/, xnsid=605cf217; domain=.renren.com; path=/, loginfrom=syshome; domain=.renren.com; path=/"}
+# url = "http://friend.renren.com/managefriends"
+# req = urllib.request.Request(url, headers=HEADERS)
+# text = urllib.request.urlopen(req)
+# print(str(text.read(), "utf-8"))
+
+
