80fe367787b6a462db2a0816665052a33504d597
shawncan <wangjiacan0216@gmail.com>
2016-11-29 20:09:01 +0800

    爬虫_糗事百科20161129

--- Cookbook/reptile_qiuhundred.py
+++ Cookbook/reptile_qiuhundred.py
@@ -3,23 +3,22 @@
 
 import urllib.request
 import urllib.parse
-import gzip
+import re
 
 page = 1
 url = "http://www.qiushibaike.com/8hr/page/" + str(page)
 headers = {
-    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N)"
-}
+    "User-Agent": "Mozilla/5.0 (windows 6.0)"}
 try:
     request = urllib.request.Request(url, headers=headers)
     response = urllib.request.urlopen(request).read()
-    # content = str(response.read(), "utf-8")
-    # print(str(response.read(), "utf-8"))
-    html = response.decode("utf-8")
-    print(html)
-    # pattern = re.compile('<span>(?:.|\n).*?</span>', re.S)
-    # items = re.findall(pattern, content)
+    html = response.decode("UTF-8")
+    # print(html)
+    pattern = re.compile('<div.*?="author clearfix">[\d\D]<a.*?>[\d\D]<img.*?>[\d\D]</a>[\d\D]<a.*?>[\d\D]<h2>(.*?)</h2>[\d\D]</a>[\d\D]<div.*?>[\d\D]</div>\n{4}<a.*?>[\d\D]<div.*?>\n{4}<span>(.*?)</span>\n{3}</div>[\d\D]</a>\n{6}<div.*?>[\d\D]<span.*?><i.*?>(.*?)</i>(.*?)</span>', re.S)
+    items = re.findall(pattern, html)
     # print(items)
+    for item in items:
+        print(item)
 except urllib.request.URLError as e:
     if hasattr(e, "code"):
         print(e.code)
